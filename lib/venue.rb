90433364969b881e521c9905b2c4c7fa2a9c3088
Regina Casias <reginaannvigil@gmail.com>
2021-03-24 14:37:26 -0600

    Completed Iterations 1 and 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,22 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    names_upcase = []
+    @patrons.each do |patron|
+      names_upcase << patron.upcase
+    end
+    names_upcase
+  end
 end
